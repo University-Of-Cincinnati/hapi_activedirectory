d4f21eb5db2b8a3b7b80b2009a40568536414755
Mikael Lindahl <mikael.lindahl@greencargo.com>
2016-10-18 10:47:31 +0200

    First version done and tested

---
+++ README.md
@@ -0,0 +1,65 @@
+[![Build Status](https://travis-ci.org/mickelindahl/hapi_redirect.svg?branch=master)](https://travis-ci.org/mickelindahl/hapi_redirect)
+[![Coverage Status](https://coveralls.io/repos/github/mickelindahl/hapi_redirect/badge.svg?branch=master)](https://coveralls.io/github/mickelindahl/hapi_redirect?branch=master)
+
+Hapi activedirectory
+====================
+
+A small [Hapi](http://hapijs.com) plugin for ldap activedirectory authentication   
+
+## Installation
+
+  npm install --save hapi-activedirectory
+
+## Usage
+```js
+'use strict'
+
+const Hapi = require( 'hapi' );
+
+const server = new Hapi.Server( { port: 3000 } );
+
+server.register( {
+    register: require( 'hapi-activedirectory' ),
+    options: { 
+        status_code: "401"
+        redirect: "/login"
+    }
+}, ( err ) => {
+    // Oh no!
+} );
+```
+
+- `options` Object or list of objects with the following keys
+    `url` Ldap server e.g. ldap://ad.domain.com:389,
+    `baseDN` base DN e.g. DC=ad,DC=domain,DC=com,
+    `domain` Ldap server domain e.g. domain
+    
+
+## Methods
+
+### `authenticate(user, password)`
+User authentication
+ 
+- `user` sAMAcountName
+- `password` passsword for account
+
+
+### `findUser(user, password)`
+Find user information
+ 
+- `user` sAMAcountName
+- `password` passsword for account
+    
+## Tests
+
+  Lab.cmd
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality. Lint and test your code.
+
+## Release History
+
+* 0.0.1 Initial release
+
--- index.js
+++ index.js
@@ -4,69 +4,179 @@
 
 'use strict';
 
-const Util=require('util');
-const ActiveDirectory =require('activedirectory');
+const ActiveDirectory = require( 'activedirectory' );
+const Promise = require( 'bluebird' );
 
-let user='s057wl';
-let pass='JBlenoir05';
+let _config;
 
-if ( !module.parent ) {
-    require( 'dotenv' ).load();
+
+/**
+ * User authentication
+ *
+ * - `user` sAMAcountName
+ * - `password` passsword for account
+ *
+ * @param {user|string}
+ * @param {user|string}
+ * @api public
+ */
+function authenticate( user, password ) {
+
+    return new Promise( ( resolve, reject )=> {
+
+        let username=user + '@'+_config.domain;
+
+        let config = {
+            url: _config.url,
+            baseDN: _config.baseDN,
+            username: username, //sAMAccountName@domain
+            password: password
+        };
+
+        let ad = new ActiveDirectory( config );
+
+        ad.authenticate( username, password, function ( err, auth ) {
+            if ( err ) {
+                return reject( err );
+            }
+
+            resolve( auth )
+
+        });
+    } )
 }
 
-var config = { url:  process.env.USER,
-    baseDN:  process.env.BASEDN,
-    username: Util.format('%s@%s.com',user, domain),
-    password: process.env.PASSWORD,
-    logging:{
-        name:'logger',
-        level:'trace'
-    }
+/**
+ * Find user information
+ *
+ * - `user` sAMAcountName
+ * - `password` passsword for account
+ *
+ * @param {user|string}
+ * @param {user|string}
+ * @api public
+ */
+function findUser( user, password ) {
+
+    return new Promise( ( resolve, reject )=> {
+
+        let username=user + '@'+_config.domain;
+
+        let config = {
+            url: _config.url,
+            baseDN: _config.baseDN,
+            username: username, //sAMAccountName@domain
+            password: password
+        };
+
+        let ad = new ActiveDirectory( config );
+
+        ad.findUser( username, function ( err, user ) {
+
+            if ( err ) {
+                return reject( err );
+            }
+
+            resolve( user )
+
+        } );
+    } )
+}
+
+exports.register = function ( server, options, next ) {
+
+    _config=options;
+
+    server.method( [
+        {
+            name: 'ad.authenticate',
+            method: authenticate,
+            options: {}
+        },
+        {
+            name: 'ad.findUser',
+            method: findUser,
+            options: {}
+        }] );
+
+    next();
 };
 
-console.log(config)
+exports.register.attributes = {
+    name: 'hapi_activedirectory',
+    version: '0.0.1'
+};
 
-var ad = new ActiveDirectory(config);
 
-// var username = 'john.smith@domain.com';
-// var password = 'password';
+// var username = 'CN=Mikael Lindahl,OU=Produktion,OU=Users,OU=Green Cargo,DC=ad,DC=greencargo,DC=com';
+// var username = 'CN=Mikael Lindahl,OU=Produktion,OU=Users,OU=Green Cargo,DC=ad,DC=greencargo,DC=com';
+// var username = 's057wl@ad.greencargo.com';
+// var username = 's059jb@ad.greencargo.com';
+//     var password = 'JBlenoir05';
+// var password = '';
 
-ad.authenticate(user, pass, function(err, auth) {
-    if (err) {
-        console.error('ERROR: '+JSON.stringify(err));
-        return;
-    }
-
-    if (auth) {
-        console.log('Authenticated!');
-    }
-    else {
-        console.log('Authentication failed!');
-    }
-});
-
-// var LdapAuth = require('ldapauth-fork');
-// var options = {
-//     url: 'ldaps://ldap.greencargo.com:636',
-//     // searchBase:'ad.greencargo.com'
-//     searchBase: "DC=ad,DC=greencargo,DC=com",
-//     searchFilter: Util.format("(sAMAccountName=%s)",user)
-// };
-// var auth = new LdapAuth(options);
+// var config = { url:   'ldap://ad.greencargo.com:389',
+//     baseDN: 'DC=ad,DC=greencargo,DC=com',
+//     // username: 'CN=Mikael Lindahl,OU=Produktion,OU=Users,OU=Green Cargo,DC=ad,DC=greencargo,DC=com',
+//     username: 's057wl@ad.greencargo.com',
+//     // username: 's057wl@ad.greencargo.com',
+//     // username: 'bajs',
+//     password: 'JBlenoir05' }
+// var ad = new ActiveDirectory(config);
 //
-// auth.authenticate(user, pass, function(err, user) {
 //
-//     if(err){
-//         console.log('err', err)
+// var username = 'CN=Mikael Lindahl,OU=Produktion,OU=Users,OU=Green Cargo,DC=ad,DC=greencargo,DC=com';
+// var username = 'CN=Mikael Lindahl,OU=Produktion,OU=Users,OU=Green Cargo,DC=ad,DC=greencargo,DC=com';
+// var username = 's057wl@ad.greencargo.com';
+// // var username = 's059jb@ad.greencargo.com';
+// var password = 'JBlenoir05';
+// // var password = '';
+//
+// ad.authenticate(username, password, function(err, auth) {
+//     if (err) {
+//         console.log('ERROR: '+JSON.stringify(err));
+//         return;
 //     }
 //
-//     console.log(user)
-//
-//     });
-//
-// auth.close(function(err) {
-//     if (err) {
-//         console.log('close',err)
+//     if (auth) {
+//         console.log('Authenticated!');
+//     }
+//     else {
+//         console.log('Authentication failed!');
 //     }
 // });
+//
+//
+// // Any of the following username types can be searched on
+// var sAMAccountName = 's057wl';
+// // var sAMAccountName = 's019zr';  // missa
+// // var sAMAccountName = 's053ya';
+// // var sAMAccountName = 's059jb'; // missa
+// // var sAMAccountName = 's103cz';
+// // var userPrincipalName = 'username@domain.com';
+// // var dn = 'CN=Smith\\, John,OU=Users,DC=domain,DC=com';
+//
+// // Find user by a sAMAccountName
+// var ad = new ActiveDirectory(config);
+// ad.findUser(sAMAccountName, function(err, user) {
+//     if (err) {
+//         console.log('ERROR: ' +JSON.stringify(err));
+//         return;
+//     }
+//
+//     if (! user) {
+//         console.log('User: ' + sAMAccountName + ' not found.');
+//     }
+//     else console.log(JSON.stringify(user));
+//
+//
+//
+//
+// });
+
+// var ad = new ActiveDirectory(config);
+
+
+
+
 
